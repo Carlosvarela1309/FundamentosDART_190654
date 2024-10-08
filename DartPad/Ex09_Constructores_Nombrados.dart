0a9e293faf85340e07f008088ab9480b7950f623
Carlos Eduardo Varela Barrios <carloseduardo.varelabarrios@utxicotepec.edu.mx>
2024-10-08 10:24:28 -0600

    Corrigiendo los archivos

--- DartPad/Ex09_Constructores_Nombrados.dart
+++ DartPad/Ex09_Constructores_Nombrados.dart
@@ -14,7 +14,6 @@ void main(){
     isActive
   )
 
-
   class Person {
     int Id,
     String cortesyTitle,
---
+++ DartPad/Ex12_Mixins.dart
@@ -0,0 +1,5 @@
+abstract class Animal{}
+
+abstract class Mamifero extends Animal{}
+abstract class Ave extends Animal{}
+abstract class Pez extends Animal{}
